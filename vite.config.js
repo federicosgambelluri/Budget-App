ea1c9a0c637c14b5ba2a46eae0d6e3124b9519db
Federico Sgambelluri <65596046+federicosgambelluri@users.noreply.github.com>
2025-12-01 10:40:28 +0100

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,9 @@ export default defineConfig({
   plugins: [react()],
   // IMPORTANTE: Se hosti su https://<USERNAME>.github.io/<REPO>, 
   // devi impostare base: '/<REPO>/'
+<<<<<<< HEAD
   // base: '/budget-app/', 
+=======
+  base: '/Budget-App/',
+>>>>>>> 7705a91 (Initial commit)
 })
